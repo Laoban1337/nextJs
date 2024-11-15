bedeface5dfd87920f16305b47606eeb2988a05e
Brian Monestime <bmonestime@gmail.com>
2024-11-14 22:14:21 -0700

    fonts added/remote

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter, Lusitana, Roboto } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+export const lusitana = Lusitana({
+  weight: "700",
+  display: "swap",
+});
+
+export const roboto = Roboto({
+  weight: "500",
+  display:'swap'
+});
